ca337139b3d7940beb6da776d1eb69ef7a08b2cc
1005903 <1005903@01HW1129022.India.TCS.com>
2017-09-20 17:03:27 +0530

    Initial commit

---
+++ TestProject/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("sudeshna");
+		String test="";
+		
+		/**
+		 * my coe
+		 */
+		
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("sudeshna");
+		
+	}
+
+}
